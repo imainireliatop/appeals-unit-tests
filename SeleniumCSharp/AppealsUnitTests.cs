279bbd3e2be83efeae884032e6b16d1984695df6
Blake A. Williams <BAWilliams@cuyahogacounty.us>
2024-09-19 09:51:21 -0400

    Added tests

--- SeleniumCSharp/AppealsUnitTests.cs
+++ SeleniumCSharp/AppealsUnitTests.cs
@@ -1,8 +1,8 @@
 using OpenQA.Selenium;
 
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Interactions;
+using System.Collections.ObjectModel;
 
 namespace SeleniumCSharp
 {
@@ -10,28 +10,31 @@ namespace SeleniumCSharp
     {
 
         IWebDriver driver;
+        ChromeOptions options;
 
         [OneTimeSetUp]
         public void Setup()
         {
             string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
-
-            driver = new ChromeDriver(path + @"\drivers\");
-            driver.Navigate().GoToUrl("https://appeals.dev.web.it.cuyahoga.cc/");
+            options = new ChromeOptions();
+            options.AddArgument("--headless=new");
+            driver = new ChromeDriver(path + @"\drivers\", options);
+  
+            driver.Navigate().GoToUrl("https://appeals.cuyahogacounty.gov/");
 
         }
 
         [Test]
         public void verifyNavbar()
         {
-            var links = driver.FindElements(By.ClassName("nav-link"));
+            ReadOnlyCollection<IWebElement> links = driver.FindElements(By.ClassName("nav-link"));
             Assert.That(driver.FindElement(By.Id("navbar")).Displayed && links.Count == 8);
         }
 
         [Test]
         public void verifyHome()
         {
-            var link = driver.FindElement(By.LinkText("HOME"));
+            IWebElement link = driver.FindElement(By.LinkText("HOME"));
             Assert.That(link.Text.Equals(" HOME"));
             link.Click();
             Assert.That(driver.FindElement(By.LinkText("More News")) != null);
@@ -40,10 +43,78 @@ namespace SeleniumCSharp
         [Test]
         public void verifyAbout()
         {
-            var link = driver.FindElement(By.LinkText("ABOUT US"));
+            IWebElement link = driver.FindElement(By.LinkText("ABOUT US"));
             Assert.That(link.Text.Equals("ABOUT US"));
             link.Click();
             Assert.That(driver.FindElements(By.TagName("h3")).Count.Equals(6));
+        }
+
+        [Test]
+        public void verifyCalendar()
+        {
+            IWebElement link = driver.FindElement(By.LinkText("CALENDAR"));
+            Assert.That(link.Text.Equals("CALENDAR"));
+            link.Click();
+            IWebElement table = driver.FindElement(By.TagName("table"));
+            string date = table.FindElement(By.TagName("a")).Text;
+            table.FindElement(By.TagName("a")).Click();
+            Assert.That(driver.FindElements(By.TagName("h1"))[1].Text == date);
+        }
+
+        [Test]
+        public void verifyEfiling()
+        {
+            IWebElement link = driver.FindElement(By.LinkText("E-FILING"));
+            Assert.That(link.Text.Equals("E-FILING"));
+            link.Click();
+            driver.SwitchTo().Window(driver.WindowHandles[1]);
+            Assert.That(driver.Title == "E-Filing");
+            // close the second tab
+            driver.Close();
+            driver.SwitchTo().Window(driver.WindowHandles[0]);
+        }
+
+        [Test]
+        public void verifyOpinions()
+        {
+            IWebElement link = driver.FindElement(By.LinkText("OPINIONS AND DOCKET"));
+            Assert.That(link.Text.Equals("OPINIONS AND DOCKET"));
+            link.Click();
+            ReadOnlyCollection<IWebElement> cards = driver.FindElements(By.ClassName("county-card"));
+            Assert.That(cards.Count == 3);
+        }
+
+        [Test]
+        public void verifyRules()
+        {
+            IWebElement link = driver.FindElement(By.LinkText("RULES"));
+            Assert.That(link.Text.Equals("RULES"));
+            link.Click();
+            ReadOnlyCollection<IWebElement> cards = driver.FindElements(By.ClassName("county-card"));
+            Assert.That(cards.Count == 2);
+        }
+
+        [Test]
+        public void verifyProSe()
+        {
+            IWebElement link = driver.FindElement(By.LinkText("PRO SE"));
+            Assert.That(link.Text.Equals("PRO SE"));
+            link.Click();
+            ReadOnlyCollection<IWebElement> cards = driver.FindElements(By.ClassName("county-card"));
+            Assert.That(cards.Count == 3);
+            link = driver.FindElement(By.LinkText("Pro Se Forms"));
+            link.Click();
+            Assert.That(driver.FindElement(By.TagName("h1")).Text == "Pro Se Forms");
+        }
+
+        [Test]
+        public void verifyForms()
+        {
+            var link = driver.FindElement(By.LinkText("FORMS"));
+            Assert.That(link.Text.Equals("FORMS"));
+            link.Click();
+            ReadOnlyCollection<IWebElement> rows = driver.FindElements(By.TagName("h3"));
+            Assert.That(rows.Count == 12);
         }
 
         [Test]
